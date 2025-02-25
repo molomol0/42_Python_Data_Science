038b2e82defbcecc28b2be8e2cfe31163029432f
Jimmy DENIS <jdenis@bess-f2r1s21.clusters.42paris.fr>
2025-02-25 08:40:01 +0100

    debut exo 4

---
+++ python_0/ex04/whatis.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def whatis(number: int):
+    return "I'm Even." if number % 2 == 0 else "I'm Odd."
+
+
+def main(argv):
+    try:
+        assert len(argv) <= 2, "more than one argument provided"
+        if len(argv) > 1:
+            assert argv[1][0] == "-" and argv[1][1:].isnumeric() \
+                or argv[1].isnumeric(), "argument is not an integer"
+            print(whatis(int(argv[1])))
+
+    except AssertionError as msg:
+        print(f'AssertionError: {msg}')
+        exit(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
