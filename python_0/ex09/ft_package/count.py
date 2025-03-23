74cd93c34cf0c6f4ee0703e24c6b4de9acb04f09
molomol0 molomol0 <you@example.com>
2025-03-23 19:27:17 +0100

    doc 09

--- python_0/ex09/ft_package/count.py
+++ python_0/ex09/ft_package/count.py
@@ -1,4 +1,9 @@
 def count_in_list(lst, str):
+    """
+    Take a list and a string
+    Count de number de string similar to the argument in the list
+    Return the result
+    """
     number = 0
     for element in lst:
         if element == str:
